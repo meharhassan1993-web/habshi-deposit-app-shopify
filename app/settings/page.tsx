edb41f4c897967056fcc1febf2548bcc3d423eac
meharhassan1993-web <meharhassan1993@gmail.com>
2026-01-16 22:31:55 +0500

    Add deposit settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,17 @@
+export default function SettingsPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Deposit Settings</h1>
+
+      <p>
+        This page will control advance payment settings.
+      </p>
+
+      <ul>
+        <li>Deposit Amount (AED)</li>
+        <li>Enable / Disable Deposit</li>
+        <li>Products eligible for deposit</li>
+      </ul>
+    </div>
+  );
+}
